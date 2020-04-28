db6ccb1c93796f02d46d3a284229ed13d914b5f1
Kelsey Skunberg <kelsey.skunberg@canonical.com>
2020-04-27 21:26:59 -0600

    IRS: add management and employees tabs

--- taxchain-dapp/src/pages/irs.js
+++ taxchain-dapp/src/pages/irs.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import {getAllEmployee, getEmployeeTotalIncome, getAllEmployeeTotalIncomeList} from "../common/contractMethods";
 import EmployeeCard from "../components/employeecard";
-import {Grid, GridColumn, GridRow} from "semantic-ui-react";
+import {Grid, GridColumn, GridRow, Table, Segment, Dimmer, Loader, Image, Icon, Statistic, Tab, Form, Message, Button, Modal, Header } from "semantic-ui-react";
 
 
 class IRS extends Component {
@@ -13,6 +13,7 @@ class IRS extends Component {
         employee:[],
         salaries : [],
         taxRate : .1,
+        errorMessage: "",
     }
 
     async componentDidMount() {
@@ -33,29 +34,81 @@ class IRS extends Component {
         }
     }
 
+    getEmployeesPane = () => {
+      let employeesPane = (
+          <div>
+            <b>All Employees</b>
+            <Grid>
+            {
+                //mapping through all employee address from state variable and setting EmployeeCard Component for each of these addresses.
+                this.state.employee.map((employeeAddress, index) => {
+                    return(<Grid.Column width={5} key={employeeAddress}><EmployeeCard addr={employeeAddress} salary={this.state.salaries[index]} taxRate={this.state.taxRate} /></Grid.Column>)
+                })
+            }
+            </Grid>
+          </div>
+      );
+      let paneName = 'Employees';
 
+      return {
+        menuItem: paneName,
+        render: () => <Tab.Pane>{employeesPane}</Tab.Pane>
+      }
+
+    }
+
+    getManagementPane = () => {
+      let managementPane = (
+        <div>
+        <Segment hidden={this.state.errorMessage === ""}>
+            {this.state.errorMessage}
+        </Segment>
+        <Segment>
+            <Statistic.Group widths='three'>
+                <Statistic>
+                    <Statistic.Value>
+                        <Icon name="dollar" />{this.state.totalSalary}
+                    </Statistic.Value>
+                    <Statistic.Label>Total Income</Statistic.Label>
+                </Statistic>
+                <Statistic>
+                    <Statistic.Value>
+                        <Icon name="percent" />{this.state.incomeTaxRate}
+                    </Statistic.Value>
+                    <Statistic.Label>Income Tax Rate</Statistic.Label>
+                </Statistic>
+
+                <Statistic>
+                    <Statistic.Value>
+                        <Icon name="dollar" />{this.state.totalTax}
+                    </Statistic.Value>
+                    <Statistic.Label>Total Income Tax</Statistic.Label>
+                </Statistic>
+            </Statistic.Group>
+        </Segment>
+        </div>
+      );
+      let paneName = 'Management';
+
+      return {
+        menuItem: paneName,
+        render: () => <Tab.Pane>{managementPane}</Tab.Pane>
+      }
+    }
 
     render() {
+        let allPanes = [
+            this.getManagementPane(),
+            this.getEmployeesPane()
+        ];
         return (
             <div>
-              <div>
-                Current Tax Due: <br/>
-                Estimated Annual Tax Due:
-              </div>
-              <div>
-                <b>All Employees</b>
-                <Grid>
-                {
-                    //mapping through all employee address from state variable and setting EmployeeCard Component for each of these addresses.
-                    this.state.employee.map((employeeAddress, index) => {
-                        return(<Grid.Column width={5} key={employeeAddress}><EmployeeCard addr={employeeAddress} salary={this.state.salaries[index]} taxRate={this.state.taxRate} /></Grid.Column>)
-                    })
-                }
-                </Grid>
-              </div>
+                meow
+                <Tab panes={allPanes} renderActiveOnly={true} />
             </div>
+
         );
-    }q
+    }
 }
 
 function mapStateToProps(state) {
